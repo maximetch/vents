89fad7742d3e9c9b6b0cf618801bf2f375368941
Norin Duke <maximetch@gmail.com>
2015-01-20 16:51:52 +0100

    Update API

--- Vents.js
+++ Vents.js
@@ -1,230 +1,33 @@
-"use strict";
+/* Copyright (c) 2015;
+ * Licensed under the MIT license */
+(function(exportName) {
+  'use strict';
 
-var Vents = {
-  /**
-   * Register a listener on a Dom element.
-   * @param {String} type The event type to listen for
-   * @param {Object} eventTarget The event target
-   * @param {Function} listener Function fired when the event occurs
-   */
-  add: function Vents_add(type, eventTarget, listener) {
-    var i, comp, eventFn, allType, eventTargets, target, finalType;
-
-    eventTargets = this._getElements(eventTarget);
-
-    for (i = 0; i < eventTargets.length; i += 1) {
-      target = eventTargets[i];
-
-      // Case of many events binded
-      if (/\,/.test(type)) {
-        allType = type.split(',');
-
-        for (i = 0; i < allType.length; i += 1) {
-          Vents.add(allType[i].trim(), eventTarget, listener);
-        }
-      } else {
-        finalType = this._getFinalEventType(type);
-        if (type === 'rclick') {
-          eventFn = function(e) {
-            if (e.which === 3) {
-              listener.call(el, e);
-            }
-
-            e.stopPropagation();
-            e.cancelBubble = true;
-
-            return false;
-          };
-
-          // Disable default context menu
-          target.oncontextmenu = function() {
-            return false;
-          };
-        }
-
-        if (!eventFn) {
-          eventFn = function(e) {
-            if (listener) {
-              listener.call(target, e);
-            }
-          };
-        }
-
-        if (!target._eventsListeners) {
-          target._eventsListeners = {};
-        }
-
-        if (!target._eventsListeners[type]) {
-          target._eventsListeners[type] = [];
-        }
-
-        target._eventsListeners[type].push({
-          listener: listener,
-          eventFn: eventFn
-        });
-
-        target.addEventListener(finalType, eventFn);
-      }
-    }
-  },
+  var IS_MOBILE = /android|iphone|ipad|ipod|iphone|mobile|tablet/i.test(navigator.userAgent);
 
   /**
-   * Register a listener on a Dom element.
-   * @param {String} type The event type to listen for
-   * @param {Object} eventTarget The event target
-   * @param {Function} listener Event listener function to be remove
-   */
-  remove: function Vents_remove(type, eventTarget, listener) {
-    var i, j, listeners, listener, defaultFn, eventTargets, target, finalType;
-
-    eventTargets = this._getElements(eventTarget);
-    finalType = this._getFinalEventType(type);
-
-    for (i = 0; i < eventTargets.length; i += 1) {
-      target = eventTargets[i];
-      listeners = this._getEventListeners(type, target);
-      j = listeners.length;
-
-      while (j--) {
-        listener = listeners[j];
-
-        if (listener === listener.listener || listener === undefined) {
-          target.removeEventListener(finalType, listener.eventFn);
-          listeners.splice(j, 1);
-        }
-      }
-    }
-  },
-
-  /**
-   * Execute the event
-   * @param {String} type The event type to execute
-   * @param {Object} eventTarget The event target
-   */
-  trigger: function Vents_trigger(type, eventTarget) {
-    var i, j, listeners, args, eventTargets, target;
-
-    args = [];
-    eventTargets = this._getElements(eventTarget);
-
-    for (i = 0; i < eventTargets.length; i += 1) {
-      target = eventTargets[i];
-      listeners = this._getEventListeners(type, target);
-
-      for (j = 0; j < arguments.length; j += 1) {
-        if (j === 0) {
-          type = arguments[j];
-        } else {
-          args.push(arguments[j]);
-        }
-      }
-
-      // Call listener load event
-      for (j = 0; j < listeners.length; j += 1) {
-        listeners[j].eventFn.apply(this, args);
-      }
-    }
-  },
-
-  /**
-   * @private
-   * Return the list of event listeners for a type of event of an eventTarget
-   * @param {String} type The event type to listen for
-   * @param {Object} eventTarget The event target
-   */
-  _getEventListeners: function _Vents_getEventListeners(type, eventTarget) {
-    var listeners, result, target;
-
-    target = this._getElements(eventTarget);
-    listeners = target._eventsListeners;
-
-    if (listeners && type) {
-      result = target._eventsListeners[type];
-    } else if (listeners) {
-      result = target._eventsListeners;
-    } else {
-      result = [];
-    }
-
-    return result;
-  },
-
-  /**
-   * @private
-   * @return {Object} Elements
-   */
-  _getElements: function _Vents_getElements() {
-    var arg = arguments[0];
-
-    return typeof arg === 'string' ? document.querySelectorAll(arg) : arg;
-  },
-
-  /**
-   * @private
-   * Check the platform
-   * @param {String} type Platform type to check
-   * @return {Boolean}
-   */
-  _isMobile: function _Vents_isMobile(type) {
-    var agents, agentCheck;
-
-    agents = ['Android', 'BlackBerry', 'iPhone|iPad|iPod', 'Opera Mini', 'IEMobile'];
-
-    switch (type) {
-    case 'Android':
-      agentCheck = agents[0];
-      break;
-
-    case 'BlackBerry':
-      agentCheck = agents[1];
-      break;
-
-    case 'iOS':
-      agentCheck = agents[2];
-      break;
-
-    case 'Opera':
-      agentCheck = agents[3];
-      break;
-
-    case 'Windows':
-      agentCheck = agents[4];
-      break;
-
-    default:
-      agentCheck = agents.join('|');
-      break;
-    }
-
-    return navigator.userAgent.match(new RegExp(agentCheck, 'i'));
-  },
-
-  /**
-   * @private
    * Get the final event type depending on the current platform
    * @param {String} type Event type
    * @return {String} Final event type
    */
-  _getFinalEventType: function _Vents_getFinalEventType(type) {
-    var isMobile, finalType;
-
-    isMobile = this._isMobile();
+  function getFinalEventType(type) {
+    var finalType;
 
     switch (type) {
       case 'vclick':
-        finalType = isMobile ? 'touchend' : 'click';
+        finalType = IS_MOBILE ? 'touchend' : 'click';
         break;
 
       case 'vmousedown':
-        finalType = isMobile ? 'touchstart' : 'mousedown';
+        finalType = IS_MOBILE ? 'touchstart' : 'mousedown';
         break;
 
       case 'vmouseup':
-        finalType = isMobile ? 'touchend' : 'mouseup';
+        finalType = IS_MOBILE ? 'touchend' : 'mouseup';
         break;
 
       case 'vmousemove':
-        finalType = isMobile ? 'touchmove' : 'mousemove';
+        finalType = IS_MOBILE ? 'touchmove' : 'mousemove';
         break;
 
       case 'mouseenter':
@@ -242,5 +45,130 @@ var Vents = {
     }
 
     return finalType;
-  },
-};
+  }
+
+  /**
+   * @constructor
+   * Events manager
+   * @param {HTMLElement} eventTarget
+   */
+  function VentsManager(eventTarget) {
+    this._eventsRegister = {};
+    this._eventTargets = typeof eventTarget === 'string' ? document.querySelectorAll(eventTarget) : eventTarget;
+  }
+
+  /**
+   * Register a listener on a Dom element.
+   * @param {String} type The event type to listen for
+   * @param {Function} listener Function fired when the event occurs
+   */
+  VentsManager.prototype.add = function(type, listener) {
+    var i, eventFn, allType, eventTarget, finalType;
+
+    for (i = 0; i < this._eventTargets.length; i += 1) {
+      eventTarget = this._eventTargets[i];
+
+      // Case of many events binded
+      if (/\,/.test(type)) {
+        allType = type.split(',');
+
+        for (i = 0; i < allType.length; i += 1) {
+          this.add(allType[i].trim(), listener);
+        }
+      } else {
+        finalType = getFinalEventType(type);
+        if (type === 'rclick') {
+          eventFn = function(e) {
+            if (e.which === 3) {
+              listener.call(eventTarget, e);
+            }
+
+            e.stopPropagation();
+            e.cancelBubble = true;
+
+            return false;
+          };
+
+          // Disable default context menu
+          eventTarget.oncontextmenu = function() {
+            return false;
+          };
+        }
+
+        if (!eventFn) {
+          eventFn = function(e) {
+            if (listener) {
+              listener.call(eventTarget, e);
+            }
+          };
+        }
+
+        if (!this._eventsRegister[type]) {
+          this._eventsRegister[type] = [];
+        }
+
+        this._eventsRegister[type].push({
+          target: eventTarget,
+          listener: listener,
+          eventFn: eventFn
+        });
+
+        eventTarget.addEventListener(finalType, eventFn);
+      }
+    }
+  };
+
+  /**
+   * Register a listener on a Dom element.
+   * @param {String} type The event type to listen for
+   * @param {Function} listener Event listener function to be remove
+   */
+  VentsManager.prototype.remove = function(type, listener) {
+    var count, register, registerItem, finalType;
+
+    finalType = getFinalEventType(type);
+    register = this.get(type);
+
+    if (register) {
+      count = register.length;
+
+      while (count--) {
+        registerItem = register[count];
+
+        if (listener === registerItem.listener || listener === undefined) {
+          registerItem.target.removeEventListener(finalType, registerItem.eventFn);
+          register.splice(count, 1);
+        }
+      }
+    }
+  };
+
+  /**
+   * Execute the event
+   * @param {String} type The event type to execute
+   */
+  VentsManager.prototype.trigger = function(type) {
+    var i, register;
+
+    register = this.get(type);
+
+    if (register) {
+      for (i = 0; i < register.length; i += 1) {
+        register[i].eventFn.call();
+      }
+    }
+  };
+
+  /**
+   * @private
+   * Return the list of event listeners for a type of event of an eventTarget
+   * @param {String} type The event type to listen for
+   * @param {Object} eventTarget The event target
+   */
+  VentsManager.prototype.get = function(type) {
+    return type ? this._eventsRegister[type] : this._eventsRegister;
+  };
+
+  window[exportName] = VentsManager;
+
+})('Vents');
